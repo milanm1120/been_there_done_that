e638c55b806025968ee65f00eab5e1c15a6d03c6
Milan <milanm1120@gmail.com>
2021-11-22 11:46:07 -0500

    login methods in helpers/application_helper.rb and controllers/application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-
+    include ApplicationHelper       #the entire application now has access to the helper methods located in ApplicationHelper
 end
