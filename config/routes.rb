b8e85294ed3e174e1023d2530a52110322b4aab2
Milan <milanm1120@gmail.com>
2021-11-22 15:24:23 -0500

    make root route welcome page and add 'root to' in config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+root to: 'sessions#welcome'
 get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
 get '/login', to: 'sessions#new'
 post 'login', to:'sessions#create'
